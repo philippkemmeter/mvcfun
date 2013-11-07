3d6911a0150cb9538509e76a0e93f16397102082
Philipp Kemmeter <phil.kemmeter@gmail.com>
2013-11-07 01:35:55 +0100

    Language may be set per controller instance
    You may now set the language for each controller. If set, the language will be
    added to the response header as 'Content-Language'. This is a SEO feature.

---
+++ test/lib/controller/Base.js
@@ -0,0 +1,47 @@
+/**
+ * Tests the controller.Base object.
+ *
+ * @author Philipp Kemmeter
+ */
+var mvcfun = require('../../../'),
+    util   = require('util'),
+    http   = require('http'),
+    os     = require('os'),
+    fs     = require('fs'),
+    should = require('should');
+
+describe('controller.Base', function() {
+    var reqMan  = new mvcfun.request.Manager(
+        new mvcfun.response.Manager(),
+        new mvcfun.request.Controller()
+    );
+    var reqCtrl = reqMan.requestController;
+    //
+    // Tests the run method
+    //
+    describe('#run', function() {
+        it('should send no Content-Language header', function(done) {
+            var resp = new http.ServerResponse({GET:'GET'});
+            resp.end = function(content) {
+                this._header.indexOf('Content-Language').should.be.equal(-1);
+                done();
+            };
+            var ctrl = new mvcfun.controller.Base('/langtest');
+            reqCtrl.addController(ctrl);
+            ctrl.run('GET', resp, '/1');
+        });
+        it('should send the correct Content-Language header', function(done) {
+            var resp = new http.ServerResponse({GET:'GET'});
+            resp.end = function(content) {
+                this._header.indexOf('Content-Language: en')
+                    .should.be.above(-1);
+                done();
+            };
+            var ctrl = new mvcfun.controller.Base('/langtest');
+            ctrl.language = 'en';
+            reqCtrl.addController(ctrl);
+            ctrl.run('GET', resp, '/1');
+        });
+    })
+});
+
